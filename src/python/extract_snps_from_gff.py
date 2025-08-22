e6c353e2456972bad2be564e0dc73994127d73e7
cjdjpj <cjdjpj@hotmail.com>
2025-08-23 00:49:16 +1000

    fix gff extraction format

--- src/python/extract_snps_from_gff.py
+++ src/python/extract_snps_from_gff.py
@@ -5,11 +5,10 @@ import sys
 warnings.filterwarnings("ignore")
 
 gwas_filename = sys.argv[1]
-gwas = pd.read_csv(gwas_filename, index_col=0)
+gwas = pd.read_csv(gwas_filename)
 num_phenotypes = gwas['phenotype'].nunique()
 
-gwas["chromosome"] = gwas.index
-gwas = gwas[gwas["chromosome"] != "intercept"]
+gwas = gwas[gwas["#chr"] != "intercept"]
 
 sig_threshold = 0.05/(len(gwas)/num_phenotypes) # bonferonni correction
 
@@ -19,22 +18,27 @@ gff_filename = sys.argv[2]
 gff_window_size = int(sys.argv[3])
 gff = pd.read_csv(gff_filename, sep="\t", header=None, comment='#')
 
-gff.columns = ["seqid", "source", "type", "start", "end", "score", "strand", "phase", "attributes"]
+gff.columns = ["#seqid", "source", "type", "start", "end", "score", "strand", "phase", "attributes"]
 
-gwas['key'] = 1
-gff['key'] = 1
-gwas['pos'] = gwas['pos'].astype(int)
-merged = pd.merge(gwas, gff, on='key').drop(columns='key')
+significant_annotations = []
+for idx, row in gwas.iterrows():
+    chr = row["#chr"]
+    pos = row["pos"]
 
-filtered = merged[
-    (merged['start'] <= merged['pos'] + gff_window_size) & 
-    (merged['end'] >= merged['pos'] - gff_window_size)
-]
+    gff_temp = gff[gff["#seqid"] == chr]
+    gff_temp = gff[gff["source"] != "RefSeq"]
+    mask1 = gff_temp["start"] <= (pos + gff_window_size)
+    mask2 = gff_temp["end"] >= (pos - gff_window_size)
+    mask = mask1 & mask2
+    gff_temp = gff_temp[mask]
+    gff_temp["phenotype"] = row["phenotype"]
 
-gff = filtered.drop(columns='pos').drop_duplicates().reset_index(drop=True)
+    significant_annotations.append(gff_temp)
+
+output_gff = pd.concat(significant_annotations, ignore_index=True)
 
 output_path = Path(gff_filename)
 output_path = output_path.with_name(output_path.stem + "_filtered" + output_path.suffix)
-gff.to_csv(output_path, sep="\t", header=False, index=False, quoting=3)
+output_gff.to_csv(output_path, sep="\t", index=False, quoting=3)
 
 print("File created: " + output_path.name, end="")
